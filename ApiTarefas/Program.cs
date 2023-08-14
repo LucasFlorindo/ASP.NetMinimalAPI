2e28fd42fadfe4f7825a814082c94fa4b1f8df6e
Lucas <lucasflorindoeg@gmail.com>
2023-08-14 16:17:20 -0300

    Methods Put and Delete

--- ApiTarefas/Program.cs
+++ ApiTarefas/Program.cs
@@ -38,6 +38,32 @@ app.MapPost("/tasks", async (Task task, AppDbContext db) =>
     return Results.Created($"/tasks/{task.Id}", task);
 });
 
+
+app.MapPut("/tasks/{id}", async (int id, Task inputTask, AppDbContext db) =>
+{
+    var task = await db.Tasks.FindAsync(id);
+    if(task is null)
+    {
+        return Results.NotFound();
+    }
+        task.Name = inputTask.Name;
+        task.IsConcluded = inputTask.IsConcluded;
+
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+});
+
+app.MapDelete("/tasks/{id}", async (int id, AppDbContext db) =>
+{
+    if(await db.Tasks.FindAsync(id) is Task task)
+    {
+        db.Tasks.Remove(task);
+        await db.SaveChangesAsync();
+        return Results.Ok(task);
+    }
+
+    return Results.NotFound();
+});
 app.Run();
 
 
