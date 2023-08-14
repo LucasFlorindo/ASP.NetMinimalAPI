dbacc634f2f76b78a0e3c8fce430155930cfb880
Lucas <lucasflorindoeg@gmail.com>
2023-08-14 14:59:52 -0300

    Map methods get and post

--- ApiTarefas/Program.cs
+++ ApiTarefas/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("TasksDb"));
 
 var app = builder.Build();
 
@@ -14,8 +18,42 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/", () => "Olá mundo!");
+app.MapGet("/", () => "Hello world!");
 
-app.MapGet("frases", async () => await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes"));
+app.MapGet("quotes", async () => await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes"));
+
+app.MapGet("/tasks", async (AppDbContext db) =>
+{
+    return await db.Tasks.ToListAsync();
+});
+
+app.MapPost("/tasks", async (Task task, AppDbContext db) =>
+{
+    db.Tasks.Add(task);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tasks/{task.Id}", task);
+});
+
 app.Run();
+
+
+class Tasks
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsConcluded { get; set; }
+
+
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Task> Tasks => Set<Task>();
+}
+
+
 
